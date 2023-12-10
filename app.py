b7c4daa0912777fbe1489fa0cd889408896cb330
naveen_msi <naveen.aifanatic@gmail.com>
2023-12-09 21:17:43 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, render_template
+
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/songs')
+def get_songs():
+    # GitHub API call to fetch MP3 files
+    # Assuming the repository's content is public
+    response = requests.get('https://api.github.com/repos/ai-fanatic/DivineRadio/contents/Audio')
+    songs = [file for file in response.json() if file['name'].endswith('.mp3')]
+    #return jsonify(songs)
+
+    # Add a mockup example of adding PDF URL for each song
+    songs_with_pdf = []
+    for song in songs:
+        song_with_pdf = song
+        song_name = song['name']
+        pdf_url = f"https://raw.githubusercontent.com/ai-fanatic/DivineRadio/main/PDF/{song_name.replace('.mp3', '.pdf')}"  # Adjust the URL pattern as needed
+        song_with_pdf['pdf_url'] = pdf_url
+        songs_with_pdf.append(song_with_pdf)
+    return jsonify(songs_with_pdf)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("search-bar");
+  const songList = document.getElementById("song-list");
+  const audioPlayer = document.getElementById("audio-player");
+  const pdfViewer = document.getElementById("pdf-viewer"); // Assuming you have an iframe with this ID
+
+  // Fetch and display songs
+  function fetchSongs() {
+    fetch("/api/songs")
+      .then((response) => response.json())
+      .then((songs) => {
+        displaySongs(songs);
+      });
+  }
+
+  function displaySongs(songs) {
+    songList.innerHTML = "";
+    songs.forEach((song) => {
+      const songDiv = document.createElement("div");
+      songDiv.textContent = song.name;
+      songDiv.onclick = () => {
+        playSong(song.download_url);
+        showPDF(song.pdf_url); // Display the PDF when the song is clicked
+      };
+      songList.appendChild(songDiv);
+    });
+  }
+  function showPDF(pdfUrl) {
+    window.open(pdfUrl, "_blank"); // Opens PDF in a new tab
+  }
+  function playSong(url) {
+    audioPlayer.src = url;
+    audioPlayer.play();
+  }
+  function showPDF(pdfUrl) {
+    const viewerUrl = `https://docs.google.com/gview?url=${encodeURIComponent(
+      pdfUrl
+    )}&embedded=true`;
+    pdfViewer.src = viewerUrl;
+    pdfViewer.style.display = "block";
+  }
+  // Search functionality
+  searchBar.addEventListener("input", function () {
+    const searchTerm = searchBar.value.toLowerCase();
+    const songs = document.querySelectorAll("#song-list div");
+    songs.forEach((song) => {
+      const songName = song.textContent.toLowerCase();
+      song.style.display = songName.includes(searchTerm) ? "" : "none";
+    });
+  });
+
+  fetchSongs();
+});
+// ... existing JavaScript code ...
+
+function toggleTheme() {
+  const body = document.body;
+  const themeIcon = document.getElementById("theme-icon");
+  const isNightMode = body.classList.toggle("night-mode");
+
+  if (isNightMode) {
+    themeIcon.textContent = "🌙";
+  } else {
+    themeIcon.textContent = "🌞";
+  }
+}
+
+// ... existing JavaScript code ...
